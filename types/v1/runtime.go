48a704dc779a5b60268a815794c15ebbcd1fdc78
cuisongliu <cuisongliu@qq.com>
2023-02-23 19:31:14 +0800

    add apply
    Signed-off-by: cuisongliu <cuisongliu@qq.com>

--- types/v1/runtime.go
+++ types/v1/runtime.go
@@ -17,8 +17,9 @@ package v1
 type RuntimeConfigDefaultComponent struct {
 	Containerd  string `json:"containerd"`
 	Docker      string `json:"docker"`
-	CRIDockerV3 string `json:"cri-docker-v3"`
-	CRIDockerV2 string `json:"cri-docker-v2"`
+	CRIDockerV3 string `json:"cri-docker-v3,omitempty"`
+	CRIDockerV2 string `json:"cri-docker-v2,omitempty"`
+	CRIDocker   string `json:"cri-docker,omitempty"`
 	Sealos      string `json:"sealos"`
 	CRIO        string `json:"crio"`
 	CRIOCrun    string `json:"crio-crun"`
